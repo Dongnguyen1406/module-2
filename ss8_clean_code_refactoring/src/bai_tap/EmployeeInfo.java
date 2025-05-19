bdd047842a4569a4523ead0cea28fffa927c505e
dongnguyen <dongntde160034@fpt.edu.vn>
2025-05-19 09:00:37 +0700

    nộp bài tâp ss8 refactoring

---
+++ ss8_clean_code_refactoring/src/bai_tap/EmployeeInfo.java
@@ -0,0 +1,17 @@
+package bai_tap;
+
+public class EmployeeInfo {
+    String name;
+    String age;
+    String awards;
+    String ctc;
+    String experience;
+
+    public EmployeeInfo(String name, String age, String awards, String ctc, String experience) {
+        this.name = name;
+        this.age = age;
+        this.awards = awards;
+        this.ctc = ctc;
+        this.experience = experience;
+    }
+}
