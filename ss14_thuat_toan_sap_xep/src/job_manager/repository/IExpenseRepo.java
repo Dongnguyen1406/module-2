8706de2644e517338df01763227774d0b623db94
dongnguyen <dongntde160034@fpt.edu.vn>
2025-05-20 15:59:10 +0700

    nộp bài tâp ss14 thuật toán sắp xếp

---
+++ ss14_thuat_toan_sap_xep/src/job_manager/repository/IExpenseRepo.java
@@ -0,0 +1,17 @@
+package job_manager.repository;
+
+import job_manager.entity.Expense;
+
+import java.util.List;
+
+public interface IExpenseRepo {
+    List<Expense> getAll();
+    void add(Expense expense);
+    boolean isExist(int expenseCode);
+    boolean delete(int expenseCode);
+    void update(Expense expense);
+    List<Expense> searchByExpenseCode(int expenseCode);
+    List<Expense> searchByExpenseName(String expenseName);
+    List<Expense> sortByNameAsc();
+    List<Expense> sortByAmountDescThenNameAsc();
+}
---
+++ ss14_thuat_toan_sap_xep/src/job_manager/view/ExpenseView.java
@@ -0,0 +1,81 @@
+package job_manager.view;
+
+import job_manager.entity.Expense;
+import job_manager.service.ExpenseService;
+import job_manager.service.IExpenseService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExpenseView {
+    public static Scanner scanner = new Scanner(System.in);
+    public static IExpenseService expenseService = new ExpenseService();
+    
+    public static void displayJob(List<Expense> expenses){
+        for (Expense expense : expenses){
+            System.out.println(expense);
+        }
+    }
+    
+    public static Expense inputExpenseInfo(){
+        int expenseCode;
+        while (true){
+            System.out.println("Nhập mã chi tiêu: ");
+            expenseCode = Integer.parseInt(scanner.nextLine());
+            if (expenseService.isExist(expenseCode)){
+                System.out.println("Mã chi tiêu đã tồn tại !");
+            } else {
+                break;
+            }
+        }
+        
+        String expenseName;
+        System.out.println("Nhập tên chi tiêu: ");
+        expenseName = scanner.nextLine();
+
+        LocalDate expenseDate;
+        System.out.println("Nhập ngày chi: ");
+        expenseDate = LocalDate.parse(scanner.nextLine());
+        
+        Double expenseAmount;
+        System.out.println("Nhập số tiền chi: ");
+        expenseAmount = Double.parseDouble(scanner.nextLine());
+        
+        String expenseDescription;
+        System.out.println("Nhập mô tả thêm: ");
+        expenseDescription = scanner.nextLine();
+        
+        return new Expense(expenseCode, expenseName, expenseDate, expenseAmount, expenseDescription);
+    }
+    
+    public static int inputExpenseCode(){
+        System.out.println("Nhập mã chi tiêu: ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static String inputExpenseName(){
+        System.out.println("Nhập tên chi tiêu: ");
+        return scanner.nextLine();
+    }
+
+    public static Expense inputExpenseInfoToUpdate(int expenseCode){
+        String expenseName;
+        System.out.println("Nhập tên chi tiêu: ");
+        expenseName = scanner.nextLine();
+
+        LocalDate expenseDate;
+        System.out.println("Nhập ngày chi: ");
+        expenseDate = LocalDate.parse(scanner.nextLine());
+
+        Double expenseAmount;
+        System.out.println("Nhập số tiền chi: ");
+        expenseAmount = Double.parseDouble(scanner.nextLine());
+
+        String expenseDescription;
+        System.out.println("Nhập mô tả thêm: ");
+        expenseDescription = scanner.nextLine();
+
+        return new Expense(expenseCode, expenseName, expenseDate, expenseAmount, expenseDescription);
+    }
+}
